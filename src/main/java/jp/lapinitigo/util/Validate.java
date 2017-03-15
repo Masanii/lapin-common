8d309c14bb87c282c99d99a1add24176c998f3db
ino <inoue.masato@tis.co.jp>
2017-03-15 14:49:28 +0900

    update invalid source code

--- src/main/java/jp/lapinitigo/util/Validate.java
+++ src/main/java/jp/lapinitigo/util/Validate.java
@@ -7,11 +7,22 @@ package jp.lapinitigo.util;
 public class Validate {
 
     /** 
-     * throw IllegalArgumentException when null or empty 
+     * throw IllegalArgumentException if not null or not empty 
      * @param str validation target
      * @param message exception message
      **/
-    public static final void isNullOrEmpty(String str, String message) {
+    public static final void isEmpty(String str, String message) {
+        if (str != null && str.length() > 0) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    /** 
+     * throw IllegalArgumentException if null or empty 
+     * @param str validation target
+     * @param message exception message
+     **/
+    public static final void isNotEmpty(String str, String message) {
         if (str == null || str.length() == 0) {
             throw new IllegalArgumentException(message);
         }
